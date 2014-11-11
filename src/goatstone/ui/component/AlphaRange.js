47d905ba19e29ff6f7a6102f1d6ab12920d03b92
goatstone <goatstone@gmail.com>
2014-11-11 15:05:42 -0800

    Refactor AlphaRange, add klass, modify scale

--- src/goatstone/ui/component/AlphaRange.js
+++ src/goatstone/ui/component/AlphaRange.js
@@ -6,137 +6,103 @@
  * Goatstone : 5.20.2014
  * */
 
-define(["PubSub", "Component", "D3"], function (PubSub, Component, d3) {
+define(["PubSub", "Component", "D3" ], function (PubSub, Component, d3) {
 
-    function AlphaRange(rootDiv) {
-        var $this = this;
-        var brushArc, brushGroup, svgTag;
-        this.width = 410;
-        this.height = 55;
-        this.extMin;
-        this.extMax;
-        //var domainMax = 3.0;
-        this.initExtent = [0, 4];
-        this.alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-        this.allElements = this.alphabet;
-        this.range = 'abc';
-        var domainMax = this.allElements.length - 1;
-        this.linearScale = d3.scale
-            .linear()
-            .domain([0, domainMax])
-            .range([15, 15 + (domainMax * 15)]);
-        //var lsi = this.linearScale.invert(10);
-        //var b = this.linearScale(1);
-        svgTag = d3.select("#alpha-range")
-            .append("svg")
-            .attr("width", this.width)
-            .attr("height", this.height);
-        // SVG group to represent the ordinal scale
-        this.ordinalGroup = svgTag.append("g");
-        this.ordinalGroup
-            .selectAll("rect")
-            .data(this.allElements)
-            .enter()
-            .append("rect")
-            .attr('class', "element-bg")
-            .attr('width', 5)
-            .attr('height', 60)
-            .attr("x", function (d, i) {
-                return $this.linearScale(i);
-            })
-            .attr("y", function (d) {
-                return 25;
-            });
-        this.ordinalGroup
-            .selectAll("text")
-            .data(this.allElements)
-            .enter()
-            .append("text")
-            .attr('class', "element-text")
-            .text(function (d) {
-                return d;
-            })
-            .attr("x", function (d, i) {
-                var scaleToFit = 1;
-                return $this.linearScale(i) * scaleToFit;
-            })
-            .attr("y", function (d) {
-                return 30;
-            })
-            .attr("class", function (d, i) {
-                if (i >= $this.initExtent[0] && i <= $this.initExtent[1]) {
-                    return 'highlight';
-                }
-                else return 'lowlight';
-            });
+    var AlphaRange = Component.extend({
+        alphaScale: null,
+        svgTag: null,
+        initExtent: [40, 56],
+        alphabet: "abcdefghijklmnopqrstuvwxyz".split(""),
+        range: 'abc',
+        selectedElements: [],
+        ordinalGroup: null,
+        brush: null,
+        xAxis: null,
+        initialize: function (rootDiv) {
+            // this.supr(rootDiv);
+            var self = this;
+            var scaleA = d3.scale.ordinal().domain(this.alphabet).rangePoints([0, 400]);
+            this.alphaScale = d3.scale.ordinal().domain(this.alphabet).range(scaleA.range());
+            this.xAxis = d3.svg.axis().scale(this.alphaScale).tickSize([25]);
+            this.svgTag = d3.select("#alpha-range")
+                .append("svg")
+                .attr("width", 415)
+                .attr("height", 55)
+                .style('padding', '0 0 0 15px');
+            this.makeBrush();
+            this.makeXAxis();
+            this.highLightTicks();
+        },
+        makeBrush: function () {
+            var self = this;
+            // Assemble the d3 brush, the selector component.  this.brush()
+            this.brush = d3.svg.brush()
+                .x(this.alphaScale)
+                .extent(this.initExtent)
+                .on("brush", function () {
+                    self.highLightTicks();
+                })
+                .on("brushstart", function () {
+                })
+                .on("brushend", function () {
+                    // snap to round value
+                    var min = Math.round(self.brush.extent()[0]);
+                    var max = Math.round(self.brush.extent()[1]);
+                    // maintain a minimum width in order to select a single character
+                    if (min === max) {
+                        max = max + 12;
+                    }
+                    self.brush.extent([min, max]);
+                    d3.select(this).call(self.brush);
+                    self.highLightTicks();
+                    PubSub.publish('alphaRange', {value: self.range });
+                });
+            var brushGroup = this.svgTag.append("g")
+                .call(this.brush);
+            var brushArc = d3.svg.arc()
+                .outerRadius(15)
+                .startAngle(0)
+                .endAngle(function (d, i) {
+                    return i ? -Math.PI : Math.PI;
+                });
+            brushGroup.selectAll(".resize").append("path")
+                .attr("transform", "translate(0, 40)")
+                .style('opacity', '.5')
+                .attr("d", brushArc);
+            brushGroup.selectAll("rect")
+                .attr("height", 35)
+                .attr("y", 25);
+        },
+        makeXAxis: function () {
+            // Add the x-axis. this.xAxis
+            this.ordinalGroup = this.svgTag.append("g")
+                .attr("transform", "translate(0, -13)")
+                .call(this.xAxis);
+            this.ordinalGroup.select('path')
+                .style('display', 'none');
+            this.ordinalGroup.selectAll('line')
+                .attr('stroke', "green");
 
-        // Assemble the d3 brush, the selector component.
-        this.brush = d3.svg.brush()
-            .x(this.linearScale)
-            .extent(this.initExtent)
-            .on("brush", function () {
-                return 1;
-            })
-            .on("brushstart", function () {
-                return 1;
-            })
-            .on("brushend", function () {
-                // snap to round value
-                var min = Math.round($this.brush.extent()[0]);
-                var max = Math.round($this.brush.extent()[1]);
-                // maintain a minimum width in order to select a single character
-                if (min === max) {
-                    max = max + 0.2;
-                }
-                d3.select(this).call($this.brush.extent([min, max]));
-                PubSub.publish('alphaRange', {value: $this.setRange()});
-                return 1;
-            });
-        brushArc = d3.svg.arc()
-            .outerRadius(15)
-            .startAngle(0)
-            .endAngle(function (d, i) {
-                return i ? -Math.PI : Math.PI;
-            });
-        brushGroup = svgTag.append("g")
-            .call(this.brush);
-        brushGroup.selectAll(".resize").append("path")
-            .attr("transform", "translate(0," + ( (this.height - 15  )  ) + ")")
-            .attr("d", brushArc);
-        brushGroup.selectAll("rect")
-            .attr("height", this.height)
-            .attr("y", 20);
-
-        $this.setRange();
-
-    }
-
-    AlphaRange.prototype = Object.create(Component.prototype);
-    AlphaRange.prototype.setRange = function () {
-        var extMin = Math.round(this.brush.extent()[0]);
-        var extMax = Math.round(this.brush.extent()[1]);
-        var res = this.allElements.slice(extMin, extMax + 1).join('');
-        this.extMin = extMin;
-        this.extMax = extMax;
-        this.range = res;
-        this.updateText();
-        return res;
-    };
-    AlphaRange.prototype.getRange = function () {
-        return this.range;
-    };
-    AlphaRange.prototype.updateText = function () {
-        var self = this;
-        this.ordinalGroup
-            .selectAll("text")
-            .attr("class", function (d, i) {
-                if (i >= self.extMin && i <= self.extMax) {
-                    return 'highlight';
-                }
-                else return 'lowlight';
-            });
-    };
+        },
+        highLightTicks: function () {
+            var self = this;
+            this.selectedElements = [];
+            this.svgTag.selectAll('text')
+                .attr("stroke", function (d, i) {
+                    var extent = self.brush.extent();
+                    var letterScale = self.alphaScale(d);
+                    if (letterScale >= extent[0] && letterScale <= (extent[1] + 3)) {
+                        self.selectedElements.push(d);
+                        return 'red';
+                    }
+                    return '#777';
+                });
+            this.range = this.selectedElements.join('');
+        },
+        getRange: function () {
+            return this.range;
+        }
+    });
 
     return AlphaRange;
 });
-
