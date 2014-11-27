09f6a76b00899cef0a22e31213786cce8c4f5a20
goatstone <jose.collas@goatstone.com>
2014-11-27 12:35:11 -0800

    Remove vendor files from repository, update bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,8 +17,6 @@ module.exports = function (grunt) {
                 run: false
             }
         },
-
-
         watch: {
             scripts: {
                 files: [
@@ -37,14 +35,6 @@ module.exports = function (grunt) {
             options: {
                 separator: ' '
             },
-//            loadfirst: {
-//                src: ['src/goatstone/ui/component/Component.js' ],
-//                dest: 'public/js/component.js'
-//            },
-//            dist: {
-//                src: ['src/goatstone/**/*.js', '!src/goatstone/ui/component/Component.js' ],
-//                dest: 'public/js/<%= pkg.name %>.js'
-//            },
             vendor: {
                 src: ['src/vendor/**/*.js'],
                 dest: 'public/js/vendor.js'
@@ -88,12 +78,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("grunt-blanket-mocha");
 
-    // Default task(s).
-    // grunt.registerTask('default', ['jshint', 'watch', 'concat', 'uglify']);
-    grunt.registerTask("tests", ["blanket_mocha"]);
-
-    // grunt.registerTask("default", ["tests"]);
-//    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask("test", ["blanket_mocha"]);
     grunt.registerTask('default', ['jshint', 'concat' ]);
-
 };
